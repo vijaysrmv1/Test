72038e40a5f1aa9284f6fb193555c4b2aaba8b36
Vijay <vvsharma36@gmail.com>
2022-09-15 22:42:06 +0530

    FirstCOmmitPOM

---
+++ src/test/java/com/example/gitgithub/GitgithubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gitgithub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitgithubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
