16d922527a1f863b5b2c407e4f01c91d85c8fadf
Vijay <vvsharma36@gmail.com>
2022-09-15 22:40:33 +0530

    FirstCOmmit

---
+++ src/main/java/com/example/gitgithub/GitgithubApplication.java
@@ -0,0 +1,13 @@
+package com.example.gitgithub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitgithubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitgithubApplication.class, args);
+    }
+
+}
